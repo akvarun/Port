8c045dd2aae752db9d5c610e19fb5140bb2148bf
Varun A K <varunak@Varuns-MacBook-Pro.local>
2024-09-10 19:42:15 -0400

    added misc

--- data/index.ts
+++ data/index.ts
@@ -50,7 +50,7 @@ export const gridItems = [
 
   {
     id: 5,
-    title: "Adapting to the newest tech stack and keeping me update",
+    title: "Adapting to the newest tech stack and keeping me update!",
     description: "The Inside Scoop",
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
