219082d32e75248b918cbfe9fe614c2c10d9aab7
akvarun <akvarun08@gmail.com>
2025-01-22 16:05:21 -0500

    changed the workspace

--- data/index.ts
+++ data/index.ts
@@ -9,7 +9,7 @@ export const navItems = [
 export const gridItems = [
   {
     id: 1,
-    title: "Workspace: Mac",
+    title: "Workspace: Linux, Mac",
     description: "",
     className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
     imgClassName: "w-full h-full",
